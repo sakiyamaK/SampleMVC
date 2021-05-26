65c4272d6da78eac5b91ca97ad4d0c1a367f4254
sakiyamaK <sakiyama.k@gmail.com>
2021-05-26 16:10:20 +0900

    test
    descriptionだってさ

--- SampleMVC/ViewController.swift
+++ SampleMVC/ViewController.swift
@@ -5,9 +5,7 @@
 //  Created by sakiyamaK on 2020/07/18.
 //  Copyright © 2020 sakiyamaK. All rights reserved.
 //
-
 import UIKit
-
 class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
